4cc281c23c401cbb200598833fe449c6755538c1
Antonio-B21 <39882567+antonio-b21@users.noreply.github.com>
2020-11-09 18:49:04 +0200

    Replies methods p2

--- Controllers/RepliesController.cs
+++ Controllers/RepliesController.cs
@@ -13,5 +13,49 @@ namespace OpenDiscussionPlatform.Controllers
         {
             return View();
         }
+
+
+
+        // GET: Edit
+        public ActionResult Edit(int id)
+        {
+            Reply reply = db.Replies.Find(id);
+
+            return View(reply);
+        }
+
+        // PUT: Edit
+        [HttpPut]
+        public ActionResult Edit(int id, Reply requestReply)
+        {
+            try
+            {
+                var reply = db.Replies.Find(id);
+                if (TryUpdateModel(reply))
+                {
+                    reply.Content = requestReply.Content;
+                    db.SaveChanges();
+                    TempData["message"] = "Raspunsul a fost modificat!";
+                    return Redirect("/Subjects/Show/" + reply.SubjectID);
+                }
+
+                return View(requestReply);
+            }
+            catch (Exception)
+            {
+                return View(requestReply);
+            }
+        }
+
+        // DELETE: Delete
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            var reply = db.Replies.Find(id);
+            db.Replies.Remove(reply);
+            db.SaveChanges();
+            TempData["message"] = "Raspunsul a fost sters cu succes!";
+            return Redirect("/Subjects/Show/" + reply.SubjectID);
+        }
     }
 }
