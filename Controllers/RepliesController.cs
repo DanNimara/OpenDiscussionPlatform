462e639f9b9f999de9703b3453f3339c8f885818
TheDoctor1963 <TheDoctor1963@users.noreply.github.com>
2020-11-09 17:45:41 +0200

    Replies methods p1

--- Controllers/RepliesController.cs
+++ Controllers/RepliesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenDiscussionPlatform.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,80 @@ namespace OpenDiscussionPlatform.Controllers
 {
     public class RepliesController : Controller
     {
-        // GET: Replies
+        private Models.AppContext db = new Models.AppContext();
+
+        // GET: Subjects
         public ActionResult Index()
         {
             return View();
+        }
+
+        // GET: Show
+        public ActionResult Show()
+        {
+            return View();
+        }
+
+        //POST: New
+        [HttpPost]
+        public ActionResult New(int id, Reply reply)
+        {
+            reply.Date = DateTime.Now;
+            reply.SubjectID = id;
+            try
+            {
+                db.Replies.Add(reply);
+                db.SaveChanges();
+                TempData["message"] = "Comentariul a fost adaugat cu succes!";
+                return Redirect("/Subjects/Show/" + reply.SubjectID);
+            }
+            catch (Exception)
+            {
+                return Redirect("/Subjects/Show/" + reply.SubjectID);
+            }
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            Subject subject = db.Subjects.Find(id);
+
+            return View(subject);
+        }
+
+        //PUT: Edit
+        [HttpPut]
+        public ActionResult Edit(int id, Subject requestSubject)
+        {
+            try
+            {
+                var subject = db.Subjects.Find(id);
+                if (TryUpdateModel(subject))
+                {
+                    subject.Title = requestSubject.Title;
+                    subject.Content = requestSubject.Content;
+                    db.SaveChanges();
+                    TempData["message"] = "Subiectul a fost modificat!";
+                    return Redirect("/Categories/Show/" + subject.CategoryID);
+                }
+
+                return View(requestSubject);
+            }
+            catch (Exception)
+            {
+                return View(requestSubject);
+            }
+        }
+
+        //DELETE: Delete
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            var subject = db.Subjects.Find(id);
+            db.Subjects.Remove(subject);
+            db.SaveChanges();
+            TempData["message"] = "Subiectul a fost sters cu succes!";
+            return Redirect("/Categories/Show/" + subject.CategoryID);
         }
     }
 }
